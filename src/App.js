0e18106f357cd791fa63fcb9a4688431ea07f6e5
unknown <konstantinosiakovougmail.com>
2023-07-11 17:17:59 +0300

    updated

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import './App.css';
+import './App.css'; 
 
 function App() {
   return (
     <div className="App">
-      <h1>HOMEPAGE</h1>
+      <meta name="description" content="your text goes here"/>
+      <meta name="og:title" content=""/>
+      <meta name="og:description" content=""/>
+      <meta name="og:image" content=""/>
+      <header><h1>HOMEPAGE</h1><img src="Logo .svg" alt="Little Lemon Logo" /></header>
+      <nav>
+        <ul>
+            <li><a href="index.html">Home</a></li>
+            <li><a href="menu.html">Menu</a></li>
+            <li><a href="book.html">Book</a></li>
+            <li><a href="about.html">About</a></li>
+        </ul>
+    </nav>
+      <main></main>
+      <footer></footer>
     </div>
   );
 }
