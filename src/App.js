825be515bc6b8223bfc1c03e30948126781bc29e
unknown <konstantinosiakovougmail.com>
2023-07-13 21:27:19 +0300

    Updated

--- src/App.js
+++ src/App.js
@@ -1,78 +1,156 @@
 import './App.css'; 
 import './styles.css';
+import React from "react";
+import { Link, Route, Routes } from "react-router-dom";
+import Booking from "./Pages/booking";
+
+// function header(){
+//   return <h1>Hello World</h1>
+// }
+
+
 function App() {
   return (
     <div className="App">
-      <link rel="stylesheet" href="css/styles.css"></link>
-      <header><h1>HOMEPAGE</h1><img src="Logo .svg" alt="Little Lemon Logo" /></header>
+      <header><div class="logo"><img src="Logo .svg" alt="Little Lemon Logo" /></div>
       <nav>
         <ul>
-            <li><a href="index.html">Home</a></li>
-            <li><a href="menu.html">Menu</a></li>
-            <li><a href="book.html">Book</a></li>
-            <li><a href="about.html">About</a></li>
+            {/* <li><Link to="/">HomePage</Link></li> */}
+            <li><Link to="/booking">Booking</Link></li>
+            {/* <li><Link to="menu.html">Menu</Link></li> 
+            <li><a href="menu.html">Reservations</a></li>
+            <li><a href="book.html">Order Online</a></li>
+            <li><a href="menu.html">Online</a></li>  */} 
         </ul>
     </nav>
+    <Routes>
+      {/* <Route path="/Booking" element={<Booking />} /> */}
+      <Route path="/booking" element={<Booking />} /> 
+    </Routes>
+    </header>
     <main>
         <section>
             <article id="hero">
-                <h1>30% Off This Weekend</h1>
+                <h1>Little Lemon</h1>
                 <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
-                    et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                    aliquip ex ea commodo consequat.
+                    Chicago<br></br>
+
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit,<br></br> sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                 </p>
+                <div class="centeredimage"><img src="restauranfood.jpg" width="200px" heigh="100px" alt="restaurant food" /></div>
+
+                <button class="button">Reserve a Table</button>
             </article>
         </section>
-
+        <h1 align="left">This week specials!    <button class="buttonOnlineMenu" align="left">Online Menu</button></h1>
         <section>
-            <article>
-                <h2>Our New Menu</h2>
-                <img src="Mario and Adrian A.jpg" alt="asset 22"/>
+            <div class="card">
+                <h2>Greek Salad $12.99</h2>
+                <img src="greek salad.jpg" width="100px" height="100px" alt="Greek Salad"/>
                 <p>
-                    Little Lemon is owned by two Italian brothers, Mario and Adrian, who moved to the United States to pursue their shared dream of owning a restaurant. 
-                    To craft the menu, Mario relies on family recipes and his experience as a chef in Italy.
-                    Adrian does all the marketing for the restaurant and led the effort to expand the menu beyond classic Italian to incorporate additional cuisines from the Mediterranean region.
+                The famous greek salad of crispy lettuce, peppers, olives and our Chicago style feta cheese, garnished with crunchy garlic and rosemary croutons. 
                 </p>
-                <a href="menu.html" target="_blank">See our new menu</a>
-            </article>
+                <a href="menu.html" target="_blank">Order a delivery</a>
+            </div>
 
-            <article>
-                <h2>Book a table</h2>
-                <img src="restaurant.jpg"/>
+            <div class="card">
+                <h2>Bruchetta  $5.99</h2>
+                <img src="bruchetta.svg" width="50%" height="50%" alt="Bruchetta"/>
                 <p>
-                    Based in Chicago, Illinois, Little Lemon is a family-owned Mediterranean restaurant, focused on traditional recipes served with a modern twist. 
-                    The chefs draw inspiration from Italian, Greek, and Turkish culture and have a menu of 12 - 15 items that they rotate seasonally.
-                    The restaurant has a rustic and relaxed atmosphere with moderate prices, making it a popular place for a meal any time of the day.
+                Our Bruschetta is made from grilled bread that has been smeared with garlic and seasoned with salt and olive oil. 
                 </p>
-                <a href="book.html" target="_blank">Book your table now</a>
-            </article>
+                <a href="book.html" target="_blank">Order a delivery</a>
+            </div>
 
-            <article>
-                <h2>Opening Hours</h2>
-                <img src="../assets/Asset 24.png"/>
+            <div class="card">
+                <h2>Lemon Desert   $5.99</h2>
+                <img src="lemon dessert.jpg" width="50%" height="50%" alt="Lemon Dessert"/>
                 <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
-                    et dolore magna aliqua.
+                This comes straight from grandma’s recipe book, every last ingredient has been sourced and is as authentic as can be imagined.
                 </p>
-                <ul>
-                    <li><b>Mon - Fri:</b> 2pm - 10pm</li>
-                    <li><b>Sat:</b> 2pm - 11pm</li>
-                    <li><b>Sun:</b> 2pm - 9pm</li>
-                </ul>
+                <a href="menu.html" target="_blank">Order a delivery</a>
+            </div>
+
+        </section>
+        <h1>Testimonials</h1>
+        <section>
+        <article>
+                <h2>Greek Salad $12.99</h2>
+                <img src="greek salad.jpg" width="100px" height="100px" alt="Greek Salad"/>
+                <p>
+                The famous greek salad of crispy lettuce, peppers, olives and our Chicago style feta cheese, garnished with crunchy garlic and rosemary croutons. 
+                </p>
+                <a href="menu.html" target="_blank">Order a delivery</a>
             </article>
+            <article>
+                <h2>Greek Salad $12.99</h2>
+                <img src="greek salad.jpg" width="100px" height="100px" alt="Greek Salad"/>
+                <p>
+                The famous greek salad of crispy lettuce, peppers, olives and our Chicago style feta cheese, garnished with crunchy garlic and rosemary croutons. 
+                </p>
+                <a href="menu.html" target="_blank">Order a delivery</a>
+            </article>
+            <article>
+                <h2>Greek Salad $12.99</h2>
+                <img src="greek salad.jpg" width="100px" height="100px" alt="Greek Salad"/>
+                <p>
+                The famous greek salad of crispy lettuce, peppers, olives and our Chicago style feta cheese, garnished with crunchy garlic and rosemary croutons. 
+                </p>
+                <a href="menu.html" target="_blank">Order a delivery</a>
+            </article>
+        </section>
+        <section>
+        <div class="littlelemon">
+                <h1>Little Lemon</h1>
+                <p>
+                    Chicago<br></br>
+
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit,<br></br> sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                </p>
+                <div class="littlelemonimage">
+                <img src="../assets/Asset 20@4x.png" alt="asset" width="10%"/>
+                </div>
+        </div>
         </section>
     </main>
-    <footer>
-        <div>
-            <img src="../assets/Asset 20@4x.png" width="10%"/>
-        </div>
-        <div>
-            <p>Copyright Little Lemon</p>
-        </div>
-    </footer>
+    <div class="footer-clean">
+        <footer>
+            <div class="container">
+            <img src="../assets/Asset 20@4x.png" alt="asset" width="10%"/>
+                <div class="row justify-content-center">
+                    <div class="col-sm-4 col-md-3 item">
+                        <h3>Doormat Navigation</h3>
+                        <ul>
+                            <li><a href="/#">Home</a></li>
+                            <li><a href="/#">About</a></li>
+                            <li><a href="/#">menu</a></li>
+                            <li><a href="/#">Reservations</a></li>
+                            <li><a href="/#">Order Online</a></li>
+                            <li><a href="/#">Login</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-sm-4 col-md-3 item">
+                        <h3>Contact</h3>
+                        <ul>
+                            <li><a href="/#">Address</a></li>
+                            <li><a href="/#">Phone number</a></li>
+                            <li><a href="/#">Email</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-sm-4 col-md-3 item">
+                        <h3>Social M edia Links</h3>
+                        <div class="col-lg-3 item social"><a href="/#"><i class="icon ion-social-facebook"></i></a><a href="/#"><i class="icon ion-social-twitter"></i></a><a href="/#"><i class="icon ion-social-snapchat"></i></a><a href="/#"><i class="icon ion-social-instagram"></i></a>
+                        <p class="copyright">Little Lemon © 2023</p>
+                    </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    </div>
     </div>
   );
+
+//   return <header/>
 }
 
 export default App;
