6391e70724890a5093fc8e7778e42f3b8c90d15f
unknown <konstantinosiakovougmail.com>
2023-07-28 20:17:07 +0300

    Last Update 28/7/23

--- src/App.test.js
+++ src/App.test.js
@@ -1,47 +1,9 @@
-import { render, fireEvent, screen } from '@testing-library/react';
-import App from './App';
-import React from 'react';
-import {Booking} from './Pages/Booking.js';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-// test('Renders the Booking', () => {
-//     render(<Booking />);
-//     const headingElement = screen.getByText("Book Now");
-//     expect(headingElement).toBeInTheDocument();
-// });
-
-describe('initializeTimes', () => {
-    it('should return the correct expected value', () => {
-      const initializeTimes = [
-        "9:00 AM",
-        "10:00 AM",
-        "11:00 AM",
-        "12:00 PM",
-        "1:00 PM",
-        "2:00 PM",
-        "3:00 PM",
-        "4:00 PM",
-        "5:00 PM",
-      ];
-        
-    });
-  });
-
-  describe('updateTimes', () => {
-    it('should return the correct expected value', () => {
-      const state = {
-        times: [
-        "9:00 AM",
-        "10:00 AM",
-        "11:00 AM",
-        "12:00 PM",
-        "1:00 PM",
-        "2:00 PM",
-        "3:00 PM",
-        "4:00 PM",
-        "5:00 PM",
-      ],
-    };
-  
-    
-    });
-  });
+test("renders learn react link", () => {
+  // render(<App />);
+  // const linkElement = screen.getByText(/learn react/i);
+  // expect(linkElement).toBeInTheDocument();
+});
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from "react-router-dom"
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-ReactDOM.render(
+root.render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
